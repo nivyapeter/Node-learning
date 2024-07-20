89a9db6f7ba2810c6a5209bbe2b66e8767a2ac7c
nivya <nivya.peter@vonnue.com>
2024-07-20 12:05:34 +0530

    npm packages

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const http = require('http');
-const fs = require('fs')
+const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+    //  lodash
+    // const num = _.random(0, 24);
+    // console.log({ num });
 
+    const greet = _.once(() => {
+        console.log('hello');
+    });
+    greet();
+    greet()
     // set header content type
     res.setHeader('Content-Type', 'text/html');
 
@@ -17,6 +25,12 @@ const server = http.createServer((req, res) => {
         case '/about':
             path += 'about.html'
             res.statusCode = 200;
+            break;
+        case '/about-bla':
+            path += 'about.html'
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
             break;
         default:
             path += '404.html'
