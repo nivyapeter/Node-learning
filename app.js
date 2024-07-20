0c8bdb470ec9a647f6e42cdb99c2c624c6cc369c
nivya <nivya.peter@vonnue.com>
2024-07-20 15:23:35 +0530

    view engines

--- app.js
+++ app.js
@@ -4,16 +4,31 @@ const express = require('express');
 
 const app = express();
 
+// register view engine
+
+app.set('view engine', 'ejs');
+
 // listen for request
 
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname });
+    // res.sendFile('./views/index.html', { root: __dirname });
+    const blogs = [{
+        title: "Yoshi", snippet: "lorem ipsum",
+        title: "finds", snippet: "lorem ipsum", 
+        title: "eggs", snippet: "lorem ipsum"
+    }]
+    res.render('index',{title:'Home',blogs})
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname });
+    // res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about')
+})
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
 })
 
 // redirect
@@ -22,6 +37,6 @@ app.get('/about-us', (req, res) => {
     res.redirect('/about')
 });
 
-app.use((req,res) => {
-res.sendFile('./views/404.html', { root: __dirname })
+app.use((req, res) => {
+    res.sendFile('./views/404.html', { root: __dirname })
 })
